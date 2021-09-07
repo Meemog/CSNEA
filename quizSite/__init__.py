bb4caacd1007461dda64d6b8f0221bcc846d4177
Oscar <oscarthomson2004@gmail.com>
2021-09-07 13:09:29 +0100

    adds comments

--- quizSite/__init__.py
+++ quizSite/__init__.py
@@ -4,26 +4,28 @@ from flask import(
         Flask, jsonify, request, abort
         )
 
-def create_app(test_config=None):
+def create_app(test_config=None): #function that creates the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'quizSite.sqlite'),
-    )
+    ) #sets config for the app including the database path and the secret key
 
-    if test_config is None:
+    if test_config is None: #loads the config if it exists
         app.config.from_pyfile('config.py', silent=True)
     else:
         app.config.from_mapping(test_config)
 
     try:
-        os.makedirs(app.instance_path)
+        os.makedirs(app.instance_path) 
+        #tries to make the files needed, but doesn't crash if they already exist
     except OSError:
         pass
 
     from . import db
 
-    @app.route('/testData', methods=['GET'])
+    @app.route('/testData', methods=['GET']) 
+    #routes /testData GET requests to this function
     def testData():
         if request.method == 'GET':
             return jsonify({'name':'bob', 'occupation':'builder'})
