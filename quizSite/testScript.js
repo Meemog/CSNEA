e9616b6cb5f6c96cf4838b4e660abd7ba57866ef
Oscar <oscarthomson2004@gmail.com>
2021-10-05 12:28:59 +0100

    tested integration of HTML with JS

--- quizSite/testScript.js
+++ quizSite/testScript.js
@@ -3,5 +3,23 @@ function doAlert() {
 }
 
 function changeTest() {
-  document.getElementById("test").innerHTML = document.getElementById("test").innerHTML + "t";
+  document.getElementById("test").innerHTML = document.getElementById("test").innerHTML + " test";
+}
+
+function textColour() {
+  var text = document.getElementById("test");
+  const colours = ["red", "orange", "yellow", "green", "blue", "purple", "black"];
+
+  text.style.color = colours[Math.floor(Math.random() * 7)];
+}
+
+function delText() {
+  var text = document.getElementById("test");
+  text.innerHTML = "text: ";
+}
+
+function submit() {
+  var field = document.getElementById("textField");
+  var text = document.getElementById("test");
+  text.innerHTML = text.innerHTML + field.value;
 }
