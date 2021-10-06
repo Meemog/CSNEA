576c9654796e7afb71136507a7623b59ac708e06
Oscar <oscarthomson2004@gmail.com>
2021-10-06 10:50:33 +0100

    Added buttons to add or remove answer slots in the form

--- quizSite/formSubmit.js
+++ quizSite/formSubmit.js
@@ -1,10 +1,29 @@
-var numAnswers = 1;
+var answerIndex = 0;
 
+function createNewAnswer() {
+  var input = document.createElement("input");
+  input.id = "Answer" + answerIndex;
+  answerIndex += 1;
+  input.placeholder = "answer";
+
+  element = document.getElementById("form1");
+  element.appendChild(input);
+}
+
+function removeAnswer(){
+  if (answerIndex != 0){
+    var lastAnswer = document.getElementById("Answer" + (answerIndex-1));
+    lastAnswer.placeholder = "removed";
+    answerIndex -= 1;
+  } else {
+    alert("Must have at least one answer");
+  }
+}
 
 function submit() {
   var username = document.getElementById("Username");
   var question = document.getElementById("Question");
-  var answer = document.getElementById("Answer");
+  var answer = document.getElementById("Answer0");
 
   var userVal = username.value;
   username.value = '';
