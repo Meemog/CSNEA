72f6c805dc2e6fa7b852799e1e78b9d09221c019
Oscar <oscarthomson2004@gmail.com>
2021-10-08 10:40:14 +0100

    fixed synax

--- quizSite/formSubmit.js
+++ quizSite/formSubmit.js
@@ -1,6 +1,6 @@
 var noAnswers = 0;
 
-function createNewAnswer() {
+function createNewAnswer() { //adds a new answer input box
   var input = document.createElement("input");
   noAnswers += 1;
   input.id = "Answer" +noAnswers ;
@@ -10,7 +10,7 @@ function createNewAnswer() {
   element.appendChild(input);
 }
 
-function removeAnswer(){
+function removeAnswer(){ //removes the last answer input box
   if (noAnswers != 1){
     var lastAnswer = document.getElementById("Answer" + (noAnswers -1));
     lastAnswer.remove();
@@ -20,7 +20,7 @@ function removeAnswer(){
   }
 }
 
-function generateNumberedStr(input) {
+function generateNumberedStr(input) { //generates a numbered list from an array
   numbered = "";
 
   for (let i = 1; i <= input.length; i ++) {
@@ -29,24 +29,24 @@ function generateNumberedStr(input) {
   return numbered
 }
 
-function submit() {
-  var username = document.getElementById("Username");
+function submit() { //looks at the data in the input fields and submits it to the api
+  var username = document.getElementById("Username"); //TODO: replace with the user currently logged in
   var question = document.getElementById("Question");
   var multiChoice = document.getElementById("multiChoice");
   var answers = [];
 
-  for (let i = 1; i <= noAnswers; i++) {
+  for (let i = 1; i <= noAnswers; i++) { //gets all the answers
     tempAns = document.getElementById("Answer" + i);
     answers.push(tempAns.value);
   }
 
   var userVal = username.value;
   var questVal = question.value;
-  var multiBool = (multiChoice.value ? 1 : 0);
+  var multiBool = (multiChoice.value ? 1 : 0); //turns JS bool value into a more universal 1 or 0
 
   alertStr = ("Posted the following to the API:\nUsername: " + userVal + "\nQuestion: " + questVal + "\nAnswer(s): " + answers + "\nMultiple choice?: " + multiBool)
 
-  if (multiBool) {
+  if (multiBool) { //prompts the user to say the correct answer
     var ansTxt = generateNumberedStr(answers);
     var correct = prompt("Which is the correct answer:\n" + ansTxt);
     alertStr += "\nCorrect index: " + correct;
