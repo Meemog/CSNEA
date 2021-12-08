484a0c88ef01f462818af039bc30690acc50217d
Oscar <oscarthomson2004@gmail.com>
2021-12-08 12:15:57 +0000

    the website can now generate questions based only on a round id

--- frontend/react-app/src/index.js
+++ frontend/react-app/src/index.js
@@ -2,77 +2,57 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const testData = {
-  "0": {
-    "Author": "TEST_USER_0",
-    "ID": 0,
-    "IsMultipleChoice": 0,
-    "Text": "2x -3 = 9 Find x"
-  },
-  "1": {
-    "Author": "TEST_USER_0",
-    "ID": 1,
-    "IsMultipleChoice": 0,
-    "Text": "x/3 * 4 = 12 Find x"
-  },
-  "2": {
-    "Author": "TEST_USER_0",
-    "ID": 2,
-    "IsMultipleChoice": 1,
-    "Text": "4x = 4 Find x",
-    "answers": {
-      "0": {
-        "AnsText": "1",
-        "ID": 2
-      },
-      "1": {
-        "AnsText": "2",
-        "ID": 3
-      },
-      "2": {
-        "AnsText": "0",
-        "ID": 4
-      }
-    }
-  }
-};
-
 class QuestionForm extends React.Component {
   constructor(props) {
     super(props);
     this.generateQuestion = this.generateQuestion.bind(this);
+    this.generateForm = this.generateForm.bind(this);
+    this.getAnswers = this.getAnswers.bind(this);
+    this.questions = []
   }
 
-  generateQuestion(id) {
+  generateQuestion(id, index) {
     const question =
-      <label>
+      <label key={index}>
         {this.props.data[id]["Text"]}
         <br />
-        <input type="text" />
+        <input type="text" id={("textInput" + index.toString())}/>
         <br />
       </label>
     return question
   }
   generateForm() {
-    let questions = [];
+    let i = 0;
+    let toRender = [];
     for (let key in this.props.data){
-      questions.push(this.generateQuestion([key]));
+      this.questions.push([this.generateQuestion([key], i), this.props.data[key]['ID']]);
+      toRender.push(this.generateQuestion([key], i))
+      i += 1;
     }
+    console.log(this.questions);
     const x = <form>
-      {questions}
-      <input type="submit" value="Submit" />
+      {toRender}
+      <input type="button" value="Submit" onClick={() => {this.getAnswers()}}/>
       </form>
 
     return x
-
+  }
+  getAnswers() {
+    for (let j=0; j < this.questions.length; j++){
+      console.log("answer: " + document.getElementById('textInput' + j.toString()).value + "\n id: " + this.questions[j][1] + "\n");
+    }
   }
 
   render() {
     return (
-      this.generateForm()
+      <div>
+        {this.generateForm()}
+      </div>
     );
   }
 }
+
+const heading = <h1>Quiz</h1>
 
 let myHeaders = new Headers();
 let myInit = { method: 'GET',
@@ -80,14 +60,24 @@ let myInit = { method: 'GET',
   mode: 'cors',
   cache: 'default'
 };
-let myRequest = new Request("http://127.0.0.1:5000/questions/0", myInit);
 
-const fetchPromise = fetch(myRequest);
+const renderForm = (id) => {
+  let myRequest = new Request(("http://127.0.0.1:5000/questions/" + id), myInit);
 
-fetchPromise
-  .then((response) => response.json())
-  .then(data => console.log(data));
+  const fetchPromise = fetch(myRequest);
 
-console.log('first');
+  fetchPromise
+    .then((response) => response.json())
+    .then(data => ReactDOM.render(<QuestionForm data={data} roundId={id} />, document.getElementById("body")));
+}
 
-//ReactDOM.render(<QuestionForm data={testData}/>, document.getElementById("root"))
+ReactDOM.render(
+  <div>
+    {heading}
+    <div id='body'>
+      <p>Enter RoundID</p>
+      <input type="text" id='roundid' />
+      <input type="button" value="Get Questions" onClick={() => {renderForm(document.getElementById('roundid').value)}} />
+    </div>
+  </div>,
+  document.getElementById('root'));
